b5ddd23958bf219bb5765d7c9a7e13a42857bacc
Joxqwe <jaffaszxc@gmail.com>
2025-07-03 01:47:02 +0800

    3rd commit

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ function calculateWinner(squares: Array<string | null>) {
     [0, 3, 6], [1, 4, 7], [2, 5, 8], 
     [0, 4, 8], [2, 4, 6]             
   ];
-  for (let [a, b, c] of lines) {
+  for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
       return squares[a];
     }
